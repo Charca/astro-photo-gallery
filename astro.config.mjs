37cd09fc1162f812551ecf26710d9fe704e91bcb
Maxi Ferreira <charca@gmail.com>
2023-08-30 00:19:25 -0700

    Initial implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  experimental: {
+    viewTransitions: true
+  },
+  adapter: cloudflare()
+});
