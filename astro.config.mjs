9d7636fb6cb2a1583cf074d798c2fc073ab96753
Maxi Ferreira <charca@gmail.com>
2023-09-02 10:33:31 -0700

    Upgrade to Astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import cloudflare from "@astrojs/cloudflare";
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  experimental: {
-    viewTransitions: true
-  },
-  adapter: cloudflare()
-});
+  adapter: cloudflare(),
+})
